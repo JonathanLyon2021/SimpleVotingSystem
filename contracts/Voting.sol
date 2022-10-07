7d3a6c3671adc5d20ef3a05a1efaacc42e64d425
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-06 23:20:23 -0400

    Update Voting.sol

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,1 +1,9 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
+contract Voting {
+
+    mapping (bytes32 => uint8) public votesReceived; // this will keep track of each candidates votes
+
+    string[] public candidateList; //this array will store the list of candidates
+}
