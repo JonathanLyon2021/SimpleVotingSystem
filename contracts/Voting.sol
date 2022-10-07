4a09b7f5525d3b47fdfe2fa30c36e8aaa1db99b0
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-06 23:20:34 -0400

    Update Voting.sol

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -6,4 +6,8 @@ contract Voting {
     mapping (bytes32 => uint8) public votesReceived; // this will keep track of each candidates votes
 
     string[] public candidateList; //this array will store the list of candidates
+    
+     function addCandidate(string memory candidateNames) public {
+        candidateList.push(candidateNames);
+    }
 }
