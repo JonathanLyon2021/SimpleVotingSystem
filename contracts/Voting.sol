b2ff45d95d81b65bc65ef5a294407220fbf99f7c
Jonathan Lyon <82686765+JonathanLyon2021@users.noreply.github.com>
2022-10-07 23:01:05 -0400

    Fixed the logic in the code so it flows correctly.

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -6,30 +6,31 @@ contract Voting {
     mapping (bytes32 => uint8) public votesReceived; // this will keep track of each candidates votes
 
     string[] public candidateList; //this array will store the list of candidates
-    
-     function addCandidate(string memory candidateNames) public {
-       //this method will add candidates to the array
-       candidateList.push(candidateNames);
+
+    function addCandidate(string memory candidateNames) public {
+        //this method will add candidates to the array
+        candidateList.push(candidateNames);
     }
-    
-     function validCandidate(string memory candidate) public view returns (bool) {
-        //this method checks whether the given candidate is contained in the array
-        for(uint i = 0; i < candidateList.length; i++) {
-         if (keccak256(bytes (candidateList[i])) == keccak256(bytes (candidate))) {
-                return true;
-            }
-        }
-    }
-    
-     function voteForCandidate(string memory candidate) public {
+
+    function voteForCandidate(string memory candidate) public {
         //this method votes for a given candidate
         require(validCandidate(candidate), "Must be valid candidate");
         votesReceived[keccak256(bytes (candidate))] += 1;
     }
-    
+
     function totalVotesFor(string memory candidate) public view returns (uint8) {
         //this method returns the total votes a candidate has received so far
         require(validCandidate(candidate), "Must be valid candidate");
         return votesReceived[keccak256(bytes (candidate))];
+    }
+
+    function validCandidate(string memory candidate) public view returns (bool) {
+        //this method checks whether the given candidate is contained in the array
+        for(uint i = 0; i < candidateList.length; i++) {
+            if (keccak256(bytes (candidateList[i])) == keccak256(bytes (candidate))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
